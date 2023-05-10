d30c69760fbf82d447a0cd9faca0c55c82de6d1f
Arabian-Mikayla <77825928+Arabian-Mikayla@users.noreply.github.com>
2023-05-09 17:05:19 -0700

    fixed code

--- Fan_Temp_Start_Stop/Fan_Temp_Start_Stop.ino
+++ Fan_Temp_Start_Stop/Fan_Temp_Start_Stop.ino
@@ -3,103 +3,27 @@
   Start and stop the fan based on temperature
 */
 
-#include "DHT.h"
-#include<LiquidCrystal.h>
+const int sensorPin = 7;
+const int fanPin = 6;
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-
-#define DHTPIN 12     //what pin we're connected to
-#define DHTTYPE DHT11   //DHT 11 
-#define pwm 9
-
-byte degree[8]={
-                0b00011,
-                0b00011,
-                0b00000,
-                0b00000,
-                0b00000,
-                0b00000,
-                0b00000,
-                0b00000
-              };
-
-// Initialize DHT sensor for normal 16mhz Arduino
-DHT dht(DHTPIN, DHTTYPE);
+int temp;
+int fanSpeed;
 
 void setup(){
-  lcd.begin(16, 2);
-  lcd.createChar(1, degree);
-  lcd.clear();
-  lcd.print("   Fan Speed  ");
-  lcd.setCursor(0,1);
-  lcd.print("  Controlling ");
-  delay(2000);
-  analogWrite(pwm, 255);
-  lcd.clear();
-  lcd.print("Robu ");
-  delay(2000);
-  Serial.begin(9600); 
-  dht.begin();
+  pinMode(fanPin, OUTPUT);
 }
 
 void loop(){
-  //Wait a few seconds between each measurement
-  delay(2000);
-
-  float h = dht.readHumidity();
-  //Read temperature as Celsius
-  float t = dht.readTemperature();
-  //Read temperature as Fahrenheit
-  float f = dht.readTemperature(true);
+  temp = analogRead(sensorPin);
+  temp = (5.0 * temp * 100.0) / 1024.0; //converting analog value to temperature in Celsius
   
-  //Check if any reads failed and exit early to try again
-  if(isnan(h) || isnan(t) || isnan(f)){
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  if(temp > 30){
+    fanSpeed = map(temp, 30, 50, 0, 255); 
+    analogWrite(fanPin, fanSpeed);
   }
-
-  //LCD setup
-  lcd.setCursor(0,0);
-  lcd.print("temp: ");
-  lcd.print(t);   //Printing terature on LCD
-  lcd.print(" C");
-  lcd.setCursor(0,1);
-   if(t <20 ){ 
-      analogWrite(9,0);
-      lcd.print("Fan OFF            ");
-      delay(100);
-    }
-    
-    else if(t==26){             
-      analogWrite(pwm, 51);
-      lcd.print("Fan Speed: 20%   ");
-      delay(100);
-    }
-    
-     else if(t==20){
-      analogWrite(pwm, 102);
-      lcd.print("Fan Speed: 40%   ");
-      delay(100);
-    }
-    
-     else if(t==28){
-      analogWrite(pwm, 153);
-      lcd.print("Fan Speed: 60%   ");
-      delay(100);
-    }
-    
-    else if(t==29){
-      analogWrite(pwm, 204);
-      
-      lcd.print("Fan Speed: 80%    ");
-      delay(100);
-    }
-
-     else if(t>29){
-      analogWrite(pwm, 255);
-      lcd.print("Fan Speed: 100%   ");
-      delay(100);
-    } 
-    
-  delay(3000);
+  else if (temp < 20){
+    analogWrite(fanPin, 0);
+  }
+  
+  delay(1000);
 }
